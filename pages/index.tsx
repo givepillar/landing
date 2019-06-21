f54dc0d6f85beb60a9a8faa1a9bb514b7027f870
Oscar <newman.oscar@gmail.com>
2019-06-21 16:39:33 -0400

    supports mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,33 +14,33 @@ const funds = [
       color: 'teal',
     },
     subscribers: '52k',
-    description: "Seals! Aren't they nifty?",
+    description: 'Seals, coral, kelp. All the good stuff.',
     image:
       'https://images.unsplash.com/photo-1496815179167-7bed3ba04811?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80',
   },
   {
-    name: 'Save the Oceans',
+    name: 'Rare Diseases',
+    numOrgs: 12,
+    category: {
+      name: 'Health & Wellness',
+      color: 'red',
+    },
+    subscribers: '9.5k',
+    description: 'Help protect the most vulnerable among us',
+    image:
+      'https://images.unsplash.com/photo-1551190822-a9333d879b1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+  },
+  {
+    name: 'The Right to Choose',
     numOrgs: 4,
     category: {
-      name: 'Environment',
+      name: 'Equality',
       color: 'purple',
     },
     subscribers: '52k',
-    description: "Seals! Aren't they nifty?",
+    description: 'Protect from attacks on choice around the country.',
     image:
-      'https://images.unsplash.com/photo-1496815179167-7bed3ba04811?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80',
-  },
-  {
-    name: 'Save the Oceans',
-    numOrgs: 4,
-    category: {
-      name: 'Environment',
-      color: 'red',
-    },
-    subscribers: '52k',
-    description: "Seals! Aren't they nifty?",
-    image:
-      'https://images.unsplash.com/photo-1496815179167-7bed3ba04811?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80',
+      'https://images.unsplash.com/photo-1488942446680-85dd7de440ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3067&q=80',
   },
 ]
 
@@ -53,11 +53,11 @@ const NavLink = ({ children, href = '/', bold = false }) => (
 const Nav = () => (
   <nav className="py-3">
     <div className="flex justify-between items-baseline">
-      <p className="logo font-display text-5xl">Pillar</p>
+      <p className="logo font-display text-4xl">Pillar</p>
 
       <ul className="flex">
-        <NavLink href="#">For Nonprofits</NavLink>
-        <NavLink href="#">Sign in</NavLink>
+        {/* <NavLink href="#">For Nonprofits</NavLink> */}
+        {/* <NavLink href="#">Sign in</NavLink> */}
         <NavLink href="#" bold>
           Get early access
         </NavLink>
@@ -74,11 +74,13 @@ const Nav = () => (
 
 const Hero = () => (
   <div className="hero">
-    <div className="container px-6 pb-16">
+    <div className="container px-6 sm:px-0 pb-16">
       <Nav />
       <div className="max-w-4xl mx-auto mt-12">
-        <h1 className="text-6xl font-black">Impact. Easy.</h1>
-        <p className="font-light text-4xl leading-normal max-w-2xl antialiased">
+        <h1 className="text-5xl leading-tight sm:text-6xl font-black">
+          Impact. Easy.
+        </h1>
+        <p className="font-light text-2xl sm:text-4xl leading-normal max-w-2xl antialiased">
           Pillar is an all new way to support the causes that matter to you.
         </p>
 
@@ -86,21 +88,39 @@ const Hero = () => (
           action="https://gives.us20.list-manage.com/subscribe/post?u=883f0f3764fd0eae45407ac87&amp;id=3f09e86dbe"
           method="post"
           target="_blank"
-          className="rounded overflow-hidden my-12 max-w-xl flex shadow-float"
+          className="block my-12"
         >
-          <input
-            type="email"
-            required
-            placeholder="Your email"
-            name="EMAIL"
-            className="flex-1 px-4 text-lg email-input"
-          />
-          <button
-            type="submit"
-            className="bg-teal-200 text-teal-800 font-bold px-8 py-3 text-lg"
-          >
-            Get early access
-          </button>
+          <div className="hidden md:block rounded overflow-hidden my-12 max-w-xl flex shadow-float">
+            <input
+              type="email"
+              required
+              placeholder="Your email"
+              name="EMAIL"
+              className="flex-1 px-4 text-lg email-input"
+            />
+            <button
+              type="submit"
+              className=" bg-teal-200 text-teal-800 font-bold px-8 py-3 text-lg"
+            >
+              Get early access
+            </button>
+          </div>
+
+          <div className="block md:hidden">
+            <input
+              type="email"
+              required
+              placeholder="Your email"
+              name="EMAIL"
+              className="block appearance-none w-full bg-white shadow-float rounded px-4 py-3 mb-4 text-lg email-input"
+            />
+            <button
+              type="submit"
+              className="w-full rounded shadow-float bg-teal-200 text-teal-800 font-bold px-8 py-3 text-lg"
+            >
+              Get early access
+            </button>
+          </div>
         </form>
       </div>
     </div>
@@ -126,27 +146,27 @@ const Hero = () => (
 )
 
 const FundCard = ({ fund }) => (
-  <div className="card bg-white rounded overflow-hidden shadow-lg hover:shadow-xl max-w-sm">
+  <div className="w-full card bg-white rounded overflow-hidden shadow-float max-w-xs">
     <div
       className="bg-cover h-20 bg-center"
       style={{ backgroundImage: `url(${fund.image})` }}
     />
     <div className="px-4 pt-4 pb-6">
-      <div className="flex justify-between items-baseline mb-1">
-        <p className="text-xl font-bold mr-6">{fund.name}</p>
+      <div className="flex justify-between items-center mb-1">
+        <p className="text-lg font-bold mr-6">{fund.name}</p>
         <span
           className={`bg-${fund.category.color}-100 text-${
             fund.category.color
-          }-900 font-medium rounded-full px-4 text-sm`}
+          }-900 font-medium rounded-full px-4 text-xs`}
           style={{ paddingTop: 2, paddingBottom: 2 }}
         >
           {fund.category.name}
         </span>
       </div>
-      <p className="uppercase tracking-wide text-sm font-medium text-gray-600 mb-4">
+      <p className="uppercase tracking-wide text-xs font-medium text-gray-600 mb-4">
         {fund.numOrgs} Nonprofits • {fund.subscribers} Subscribers
       </p>
-      <p className="text-lg">{fund.description}</p>
+      <p className="text-sm text-gray-700">{fund.description}</p>
     </div>
     <style jsx>{`
       .card {
@@ -166,9 +186,13 @@ const FundCard = ({ fund }) => (
 )
 
 const FundList = ({ funds }) => (
-  <div>
+  <div className="flex flex-col items-center">
     {funds.map((fund, i) => (
-      <div className={'card ' + (i == 1 ? 'active' : '')}>
+      <div
+        className={
+          'card -my-2 w-full flex justify-center ' + (i == 1 ? 'active' : '')
+        }
+      >
         <FundCard fund={fund} />
       </div>
     ))}
@@ -176,6 +200,11 @@ const FundList = ({ funds }) => (
       .card:not(.active) {
         transform: scale(0.9);
         opacity: 0.8;
+        z-index: 0;
+      }
+
+      .card.active {
+        z-index: 1;
       }
     `}</style>
   </div>
@@ -193,7 +222,9 @@ const SectionCopy = ({ theme, title, subtitle, children }) => (
 )
 
 const SectionMedia = ({ children }) => (
-  <div className="md:w-1/2 order-first md:order-none">{children}</div>
+  <div className="w-full max-w-xs mx-auto md:w-1/2 mb-12 sm:mb-0 order-first md:order-none">
+    {children}
+  </div>
 )
 
 const Panel = ({ media, copy, flip = false }) => (
@@ -257,24 +288,7 @@ const Home = () => (
   <div className="w-full">
     <Head />
     <Hero />
-    {/* <div className="mb-40">
-      <div className="container">
-        <FundList funds={funds} />
-      </div>
-      <div className="py-20">
-        <div className="container px-6">
-          <SectionCopy
-            title="Give to causes directly."
-            subtitle="We'll do the heavy lifting."
-            theme="teal"
-          >
-            Pick the causes you care about and give to expertly curated bundles
-            of charities doing the best, most impactful work. We make sure every
-            charity meets our high standards.
-          </SectionCopy>
-        </div>
-      </div>
-    </div> */}
+
     <div className="container px-6 overflow-hidden">
       <Panel
         media={
@@ -288,9 +302,10 @@ const Home = () => (
             subtitle="We'll do the heavy lifting."
             theme="teal"
           >
-            Pick the causes you care about and give to expertly curated bundles
-            of charities doing the best, most impactful work. We make sure every
-            charity meets our high standards.
+            You have causes on your mind. Let us help you translate them into
+            action. We have experts curate bundles of charities to represent the
+            causes that are on your mind. All you need to do is find what
+            resonates with you.
           </SectionCopy>
         }
       />
@@ -309,9 +324,8 @@ const Home = () => (
             subtitle="All in one place."
             theme="red"
           >
-            Pick the causes you care about and give to expertly curated bundles
-            of charities doing the best, most impactful work. We make sure every
-            charity meets our high standards.
+            Instead of scattering your giving all across the web. See the
+            difference each dollar is making from one platform
           </SectionCopy>
         }
       />
@@ -329,9 +343,10 @@ const Home = () => (
             subtitle="With security from the start."
             theme="purple"
           >
-            Pick the causes you care about and give to expertly curated bundles
-            of charities doing the best, most impactful work. We make sure every
-            charity meets our high standards.
+            We carefully vet nonprofits 990 forms and refer to Charity
+            Navigator's ratings, the industry standard, to connect you with
+            organizations you can believe in. And best of all, we can safeguard
+            your information, so you will not be solicited every other day.
           </SectionCopy>
         }
       />
